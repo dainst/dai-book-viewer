0552613824ba7e471c9b3ce98e610ab9283fb8d1
Philipp Franck <philipp.franck@dainst.de>
2017-01-20 12:54:57 +0100

    improve build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -72,29 +72,7 @@ function stripUMDHeaders(content) {
     '\\} else ', 'g');
   return content.replace(reg, '');
 }
-/*
-function checkChromePreferencesFile(chromePrefsPath, webPrefsPath) {
-  var chromePrefs = JSON.parse(fs.readFileSync(chromePrefsPath).toString());
-  var chromePrefsKeys = Object.keys(chromePrefs.properties);
-  chromePrefsKeys.sort();
-  var webPrefs = JSON.parse(fs.readFileSync(webPrefsPath).toString());
-  var webPrefsKeys = Object.keys(webPrefs);
-  webPrefsKeys.sort();
-  var telemetryIndex = chromePrefsKeys.indexOf('disableTelemetry');
-  if (telemetryIndex >= 0) {
-    chromePrefsKeys.splice(telemetryIndex, 1);
-  } else {
-    console.log('Warning: disableTelemetry key not found in chrome prefs!');
-    return false;
-  }
-  if (webPrefsKeys.length !== chromePrefsKeys.length) {
-    return false;
-  }
-  return webPrefsKeys.every(function (value, index) {
-    return chromePrefsKeys[index] === value &&
-           chromePrefs.properties[value].default === webPrefs[value];
-  });
-}*/
+
 
 function bundle(filename, outfilename, pathPrefix, initFiles, amdName, defines, isMainFile, versionInfo) {
   // Reading UMD headers and building loading orders of modules. The
@@ -244,56 +222,7 @@ function createDbvBundle(defines) {
 	return source;
 }
 
-/*
-function createWebBundle(defines) {
-  var versionJSON = JSON.parse(
-    fs.readFileSync(BUILD_DIR + 'version.json').toString());
 
-  var template, files, outputName, amdName;
-  if (defines.COMPONENTS) {
-    amdName = 'pdfjs-dist/web/pdf_viewer';
-    template = 'pdf.js/web/pdf_viewer.component.js';
-    files = [
-      'pdf_viewer.js',
-      'pdf_history.js',
-      'pdf_find_controller.js',
-      'download_manager.js'
-    ];
-    outputName = 'pdf_viewer.js';
-  } else {
-
-
-    amdName = 'pdfjs-dist/web/viewer';
-    outputName = 'viewer.js';
-    template = 'pdf.js/web/viewer.js';
-    files = ['app.js'];
-    if (defines.FIREFOX || defines.MOZCENTRAL) {
-      files.push('firefoxcom.js');
-    } else if (defines.CHROME) {
-      files.push('chromecom.js', 'mozPrintCallback_polyfill.js');
-    } else if (defines.GENERIC) {
-      files.push('mozPrintCallback_polyfill.js');
-    }
-  }
-
-  var source = stream.Readable({ objectMode: true });
-  source._read = function () {
-    // 'buildnumber' shall create BUILD_DIR for us
-    var tmpFile = BUILD_DIR + '~' + outputName + '.tmp';
-    bundle(template, tmpFile, 'web/', files, amdName, defines, false,
-      versionJSON);
-    this.push(new gutil.File({
-      cwd: '',
-      base: '',
-      path: outputName,
-      contents: fs.readFileSync(tmpFile)
-    }));
-    fs.unlinkSync(tmpFile);
-    this.push(null);
-  };
-  return source;
-}
-*/
 function checkFile(path) {
   try {
     var stat = fs.lstatSync(path);
@@ -302,62 +231,9 @@ function checkFile(path) {
     return false;
   }
 }
-/*
-function createTestSource(testsName) {
-  var source = stream.Readable({ objectMode: true });
-  source._read = function () {
-    console.log();
-    console.log('### Running ' + testsName + ' tests');
 
-    var PDF_TEST = process.env['PDF_TEST'] || 'test_manifest.json';
-    var PDF_BROWSERS = process.env['PDF_BROWSERS'] ||
-      'resources/browser_manifests/browser_manifest.json';
 
-    if (!checkFile('test/' + PDF_BROWSERS)) {
-      console.log('Browser manifest file test/' + PDF_BROWSERS +
-                  ' does not exist.');
-      console.log('Copy and adjust the example in ' +
-                  'test/resources/browser_manifests.');
-      this.emit('error', new Error('Missing manifest file'));
-      return null;
-    }
 
-    var args = ['test.js'];
-    switch (testsName) {
-      case 'browser':
-        args.push('--reftest', '--manifestFile=' + PDF_TEST);
-        break;
-      case 'browser (no reftest)':
-        args.push('--manifestFile=' + PDF_TEST);
-        break;
-      case 'unit':
-        args.push('--unitTest');
-        break;
-      case 'font':
-        args.push('--fontTest');
-        break;
-      default:
-        this.emit('error', new Error('Unknown name: ' + testsName));
-        return null;
-    }
-    args.push('--browserManifestFile=' + PDF_BROWSERS);
-
-    var testProcess = spawn('node', args, {cwd: TEST_DIR, stdio: 'inherit'});
-    testProcess.on('close', function (code) {
-      source.push(null);
-    });
-  };
-  return source;
-}
-
-gulp.task('default', function() {
-  console.log('Available tasks:');
-  var tasks = Object.keys(gulp.tasks);
-  tasks.sort();
-  tasks.forEach(function (taskName) {
-    console.log('  ' + taskName);
-  });
-});
 
 /**
  * dbv: finds out the build number with git
@@ -406,59 +282,6 @@ function getVersionInfo(config, target, readyFn) {
 
 
 
-/*
-gulp.task('bundle-firefox', ['buildnumber'], function () {
-  var defines = builder.merge(DEFINES, {FIREFOX: true});
-  return streamqueue({ objectMode: true },
-    createBundle(defines), createWebBundle(defines))
-    .pipe(gulp.dest(BUILD_DIR));
-});
-
-gulp.task('bundle-mozcentral', ['buildnumber'], function () {
-  var defines = builder.merge(DEFINES, {MOZCENTRAL: true});
-  return streamqueue({ objectMode: true },
-    createBundle(defines), createWebBundle(defines))
-    .pipe(gulp.dest(BUILD_DIR));
-});
-
-gulp.task('bundle-chromium', ['buildnumber'], function () {
-  var defines = builder.merge(DEFINES, {CHROME: true});
-  return streamqueue({ objectMode: true },
-    createBundle(defines), createWebBundle(defines))
-    .pipe(gulp.dest(BUILD_DIR));
-});
-*/
-/*
-gulp.task('bundle-singlefile', ['buildnumber'], function () {
-  var defines = builder.merge(DEFINES, {SINGLE_FILE: true});
-  return createBundle(defines).pipe(gulp.dest(BUILD_DIR));
-});
-
-gulp.task('bundle-generic', ['buildnumber'], function () {
-  var defines = builder.merge(DEFINES, {GENERIC: true});
-  return streamqueue({ objectMode: true },
-    createBundle(defines), createWebBundle(defines))
-    .pipe(gulp.dest(BUILD_DIR));
-});
-
-gulp.task('bundle-minified', ['buildnumber'], function () {
-  var defines = builder.merge(DEFINES, {MINIFIED: true, GENERIC: true});
-  return streamqueue({ objectMode: true },
-    createBundle(defines), createWebBundle(defines))
-    .pipe(gulp.dest(BUILD_DIR));
-});
-*/
-/*
-gulp.task('bundle-components', ['buildnumber'], function () {
-  var defines = builder.merge(DEFINES, {COMPONENTS: true, GENERIC: true});
-  return createWebBundle(defines).pipe(gulp.dest(BUILD_DIR));
-});
-
-gulp.task('bundle', ['buildnumber'], function () {
-  return createBundle(DEFINES).pipe(gulp.dest(BUILD_DIR));
-});
-*/
-
 gulp.task('bundle-dbv', ['buildnumber_dbv', 'buildnumber_pdfjs'], function () {
   var defines = builder.merge(DEFINES, {GENERIC: true});
   return streamqueue(
@@ -469,165 +292,11 @@ gulp.task('bundle-dbv', ['buildnumber_dbv', 'buildnumber_pdfjs'], function () {
     .pipe(gulp.dest(BUILD_DIR));
 });
 
-gulp.task('test', function () {
-  return streamqueue({ objectMode: true },
-    createTestSource('unit'), createTestSource('browser'));
-});
-/*
-gulp.task('bottest', function () {
-  return streamqueue({ objectMode: true },
-    createTestSource('unit'), createTestSource('font'),
-    createTestSource('browser (no reftest)'));
-});
-*/
-gulp.task('browsertest', function () {
-  return createTestSource('browser');
+
+
+
+gulp.task('dbv', function() {
+  global.target.dbv();
 });
 
-gulp.task('browsertest-noreftest', function () {
-  return createTestSource('browser (no reftest)');
-});
 
-gulp.task('unittest', function () {
-  return createTestSource('unit');
-});
-
-gulp.task('fonttest', function () {
-  return createTestSource('font');
-});
-/*
-gulp.task('botmakeref', function (done) {
-  console.log();
-  console.log('### Creating reference images');
-
-  var PDF_BROWSERS = process.env['PDF_BROWSERS'] ||
-    'resources/browser_manifests/browser_manifest.json';
-
-  if (!checkFile('test/' + PDF_BROWSERS)) {
-    console.log('Browser manifest file test/' + PDF_BROWSERS +
-      ' does not exist.');
-    console.log('Copy and adjust the example in ' +
-      'test/resources/browser_manifests.');
-    done(new Error('Missing manifest file'));
-    return;
-  }
-
-  var args = ['test.js', '--masterMode', '--noPrompts',
-              '--browserManifestFile=' + PDF_BROWSERS];
-  var testProcess = spawn('node', args, {cwd: TEST_DIR, stdio: 'inherit'});
-  testProcess.on('close', function (code) {
-    done();
-  });
-});
-*/
-gulp.task('lint', function (done) {
-  console.log();
-  console.log('### Linting JS files');
-
-  // Lint the Firefox specific *.jsm files.
-  var options = ['node_modules/jshint/bin/jshint', '--extra-ext', '.jsm', '.'];
-  var jshintProcess = spawn('node', options, {stdio: 'inherit'});
-  jshintProcess.on('close', function (code) {
-    if (code !== 0) {
-      done(new Error('jshint failed.'));
-      return;
-    }
-
-    console.log();
-    console.log('### Checking UMD dependencies');
-    var umd = require('./external/umdutils/verifier.js');
-    if (!umd.validateFiles({'pdfjs': './src', 'pdfjs-web': './web'})) {
-      done(new Error('UMD check failed.'));
-      return;
-    }
-
-    console.log();
-    console.log('### Checking supplemental files');
-
-    if (!checkChromePreferencesFile(
-          'extensions/chromium/preferences_schema.json',
-          'web/default_preferences.json')) {
-      done(new Error('chromium/preferences_schema is not in sync.'));
-      return;
-    }
-
-    console.log('files checked, no errors found');
-    done();
-  });
-});
-
-gulp.task('server', function (done) {
-  console.log();
-  console.log('### Starting local server');
-
-  var WebServer = require('./test/webserver.js').WebServer;
-  var server = new WebServer();
-  server.port = 8888;
-  server.start();
-});
-
-gulp.task('clean', function(callback) {
-  console.log();
-  console.log('### Cleaning up project builds');
-
-  rimraf(BUILD_DIR, callback);
-});
-
-gulp.task('makefile', function () {
-  var makefileContent = 'help:\n\tgulp\n\n';
-  var targetsNames = [];
-  for (var i in target) {
-    makefileContent += i + ':\n\tgulp ' + i + '\n\n';
-    targetsNames.push(i);
-  }
-  makefileContent += '.PHONY: ' + targetsNames.join(' ') + '\n';
-  return createStringSource('Makefile', makefileContent)
-    .pipe(gulp.dest('.'));
-});
-
-gulp.task('importl10n', function(done) {
-  var locales = require('./pdf.js/external/importL10n/locales.js');
-
-  console.log();
-  console.log('### Importing translations from mozilla-aurora');
-
-  if (!fs.existsSync(L10N_DIR)) {
-    fs.mkdirSync(L10N_DIR);
-  }
-  locales.downloadL10n(L10N_DIR, done);
-});
-
-// Getting all shelljs registered tasks and register them with gulp
-var gulpContext = false;
-for (var taskName in global.target) {
-  if (taskName in gulp.tasks) {
-    continue;
-  }
-
-  var task = (function (shellJsTask) {
-    return function () {
-      gulpContext = true;
-      try {
-        shellJsTask.call(global.target);
-      } finally {
-        gulpContext = false;
-      }
-    };
-  })(global.target[taskName]);
-  gulp.task(taskName, task);
-}
-
-Object.keys(gulp.tasks).forEach(function (taskName) {
-
-  var oldTask = global.target[taskName] || function () {
-    gulp.run(taskName);
-  };
-
-  global.target[taskName] = function (args) {
-    // The require('shelljs/make') import in make.js will try to execute tasks
-    // listed in arguments, guarding with gulpContext
-    if (gulpContext) {
-      oldTask.call(global.target, args);
-    }
-  };
-});
